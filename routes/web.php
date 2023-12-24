5c8e668f30da7109576ba2265e27fa623d9484f0
antonizsar13 <antonizsar13@gmail.com>
2023-12-24 11:21:14 +0300

    123

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{id?}/', function ($id = 0) {
+    return "User id: $id";
+});
